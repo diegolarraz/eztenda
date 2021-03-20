8d841f4b34a8c2b0254bbf636aa0c17d5e7f23f6
Ben McLaren <benmclaren@live.co.uk>
2021-03-20 18:19:23 +0000

    home styling

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -7,12 +7,12 @@ import Button from "../../components/Button/BrandButton.js"
 
 const Home = () => {
   return (
-    <div className="Hero-container m-auto">
+    <div className="Section-Container m-auto">
       <div className="flex flex-col justify-center">
         <h1 className="text-7xl font-bold Hero-title">
           A great deal <span className="border-bottom">better</span>
         </h1>
-        <p className="mt-9 mb-9 font-thin Hero-insert">
+        <p className="mt-14 mb-14 font-thin Hero-insert">
         An online auction where bars can tender their listings and deals and drinks brands can bid for them.
         </p>  
         <Button />
