3d0202adfcb9488d695ba4cf7fd0372a5e25f9ed
johnmarsden24 <john.marsden24@gmail.com>
2021-03-20 13:13:41 +0000

    fixing filenames

--- src/pages/listings/listings.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-const Listings = () => <h1>This is the listings page</h1>;
-
-export default Listings;
--- src/pages/profile/profile.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-const Profile = () => <h1>This is the profile page</h1>;
-
-export default Profile;
