798804eee37e8b8a703fd6692623e6b25983caf9
Ben McLaren <benmclaren@live.co.uk>
2021-03-20 22:32:34 +0000

    listing

--- src/pages/Listing/Listing.js
+++ src/pages/Listing/Listing.js
@@ -3,6 +3,8 @@ import { useParams } from "react-router-dom";
 
 const Listing = () => {
   const { id } = useParams();
+  console.log(useParams());
+
 
   return <h1>This is the listing page for - {id}</h1>;
 };
