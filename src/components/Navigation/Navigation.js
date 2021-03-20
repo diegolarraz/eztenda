a9d2e927bafda80eac2d09581b0abcfbcc93ca84
johnmarsden24 <john.marsden24@gmail.com>
2021-03-20 16:07:48 +0000

    made mobile navbar

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,25 +1,46 @@
-import React from "react";
+import React, { Component } from "react";
 // import classes from "./NavigationItems.module.css";
 import NavigationItem from "./NavigationItem/NavigationItem";
 import classes from "./Navigation.module.css";
 
 import eztendaLogo from "../../assets/eztenda-logo.png";
 
-const navigationItems = () => (
-  <nav className={classes.Navigation}>
-    <NavigationItem link="/">
-      <img
-        src={eztendaLogo}
-        alt="eztenda logo"
-        className={classes.NavigationImg}
-      />
-    </NavigationItem>
+class Navigation extends Component {
+  state = {
+    menuOpen: false,
+  };
 
-    <ul className={classes.NavigationLinks}>
-      <NavigationItem link="/listings">Listings</NavigationItem>
-      <NavigationItem link="/profile">Profile</NavigationItem>
-    </ul>
-  </nav>
-);
+  handleClick = () => {
+    this.setState({ menuOpen: !this.state.menuOpen });
+  };
 
-export default navigationItems;
+  render() {
+    let navigationLinksClasses = this.state.menuOpen
+      ? `${classes.Links} ${classes.LinksActive}`
+      : classes.Links;
+
+    let hamburgerClasses = this.state.menuOpen
+      ? `${classes.Hamburger} ${classes.Active}`
+      : classes.Hamburger;
+
+    return (
+      <nav className={classes.Container}>
+        <NavigationItem link="/">
+          <img src={eztendaLogo} alt="eztenda logo" className={classes.Img} />
+        </NavigationItem>
+        <div className={hamburgerClasses} onClick={this.handleClick}>
+          <span className={classes.Line}></span>
+          <span className={classes.Line}></span>
+          <span className={classes.Line}></span>
+        </div>
+
+        <ul className={navigationLinksClasses}>
+          <NavigationItem link="/listings">Listings</NavigationItem>
+          <NavigationItem link="/profile">Profile</NavigationItem>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+export default Navigation;
