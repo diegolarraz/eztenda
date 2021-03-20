dff479a73e7cf19f77ececc4d992bb80439a69fc
johnmarsden24 <john.marsden24@gmail.com>
2021-03-20 13:15:13 +0000

    fixed filenames

---
+++ src/pages/Listing/Listing.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+const Listing = () => {
+  const { id } = useParams();
+
+  return <h1>This is the listing page for - {id}</h1>;
+};
+
+export default Listing;
---
+++ src/pages/Profile/Profile.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Profile = () => <h1>This is the profile page</h1>;
+
+export default Profile;
