aee354dd78bca609b50f34a2713889c3fe29428c
Ben McLaren <benmclaren@live.co.uk>
2021-03-20 15:41:08 +0000

    added tailwind

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,5 +1,24 @@
 import React from "react";
+import beer from "../../assets/beer.svg";
 
-const Home = () => <h1>This is the home page</h1>;
+
+
+const Home = () => {
+  return (
+    <div>
+      <div>
+        <h1>
+          A great deal better
+        </h1>
+        <p>
+        An online auction where bars can tender their listings and deals and drinks brands can bid for them.
+        </p>
+      </div>
+      <img src={beer}></img>
+
+      
+    </div>
+  );
+};
 
 export default Home;
