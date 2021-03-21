f70ee37cda0ffab58bb81e51054e3e9c0e9c667e
Ben McLaren <benmclaren@live.co.uk>
2021-03-21 10:17:08 +0000

    data on listing show page

--- src/pages/Listing/Listing.js
+++ src/pages/Listing/Listing.js
@@ -1,12 +1,31 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { useParams } from "react-router-dom";
+import api from "../../axiosInstances";
 
 const Listing = () => {
+  const [listing, setListing] = useState({})
   const { id } = useParams();
-  console.log(useParams());
+
+  useEffect(() => {
+    const getListings = async () => {
+      try {
+        const response = await api.get(`/listings/${id}`);
+        setListing(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getListings();
+  }, []);
+
+  console.log(listing.category);
 
 
-  return <h1>This is the listing page for - {id}</h1>;
+  return (
+    <>
+      
+    </>
+  );
 };
 
 export default Listing;
